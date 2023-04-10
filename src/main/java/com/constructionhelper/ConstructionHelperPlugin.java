ca155b5cfea5eb6dcb3c30194c51e6a6e15bd919
svenfulen <sfulen99@gmail.com>
2023-04-09 22:09:53 -0500

    setup project

--- src/main/java/com/constructionhelper/ConstructionHelperPlugin.java
+++ src/main/java/com/constructionhelper/ConstructionHelperPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.constructionhelper;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class ConstructionHelperPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private ConstructionHelperConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	ConstructionHelperConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(ConstructionHelperConfig.class);
 	}
 }
